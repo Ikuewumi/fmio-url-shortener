8881c0ed185ebfac87565936fd93b839a5a95c57
Ayobami <ayobamsik@gmail.com>
2022-12-31 17:16:46 +0100

    finished designing the ui

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-import './app.scss';
+import "./index.css"
+
+
+
+// Setting up nav {}
+
+const menuToggle = document.querySelector('#menu-toggle')
+const header = document.querySelector('header')
+menuToggle.addEventListener('click', () => { header.classList.toggle('on') })
+
+
+
+
---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: [
+      "./index.html"
+   ],
+   theme: {
+      screens: {
+         sm: '480px',
+         md: '768px',
+         lg: '970px',
+         xl: '1440px'
+      },
+      extend: {
+         colors: {
+            cyan: 'hsl(180, 66%, 49%)',
+            darkViolet: 'hsl(257, 27%, 26%)',
+            red: 'hsl(0, 87%, 67%)',
+            gray: 'hsl(0, 0%, 75%)',
+            darkGray: 'hsl(0, 0%, 45%)',
+            grayishViolet: 'hsl(257, 7%, 63%)',
+            veryDarkBlue: 'hsl(255, 11%, 22%)',
+            veryDarkViolet: 'hsl(260, 8%, 14%)'
+         },
+
+         // fontSize: '18px'
+      },
+   },
+   plugins: [],
+}
